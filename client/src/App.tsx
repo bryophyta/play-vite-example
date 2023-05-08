f2edbd32d57e48327dd70d783357bdd6b4429024
Pete F <pete.faulconbridge@theguardian.com>
2023-05-08 15:33:45 +0100

    Tidy React template, install @emotion/react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(1)
+export function App() {
+  const [count, setCount] = useState(1);
 
   return (
-    <div className="App">
-      <div>
+    <div>
+      <header>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </header>
+      <h1>Play, Vite & React Starter Project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +28,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
-
-export default App
